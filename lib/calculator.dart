5e7ad5df357d416aa20aa8b6117efdbbcec97e11
JITHU MATHEWS <jithumathews111@gmail.com>
2023-03-21 11:53:25 +0530

    Initial commit

---
+++ lib/calculator.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String userInput = '';
+  String result = '0';
+
+  List<String> ButtonList = [
+    'AC',
+    '(',
+    ')',
+    '/',
+    '7',
+    '8',
+    '9',
+    '*',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '-',
+    'C',
+    '0',
+    '.',
+    '=',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    userInput,
+                    style: TextStyle(fontSize: 32, color: Colors.white),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    result,
+                    style: TextStyle(
+                        fontSize: 48,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Divider(
+            color: Colors.white,
+          ),
+          Expanded(
+              child: Container(
+            padding: EdgeInsets.all(10),
+            child: GridView.builder(
+                itemCount: ButtonList.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 12,
+                    mainAxisSpacing: 12),
+                itemBuilder: (BuildContext context, int index) {
+                  return CustomButton(ButtonList[index]);
+                }),
+          ))
+        ],
+      ),
+    );
+  }
+
+  Widget CustomButton(String text) {
+    return InkWell(
+      splashColor: Colors.black,
+      onTap: () {
+        setState(() {
+          handleButton(text);
+        });
+      },
+      child: Ink(
+        decoration: BoxDecoration(
+            color: getBgColor(text),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white,
+                blurRadius: 4,
+                spreadRadius: .5,
+                offset: Offset(-3, -3),
+              )
+            ]),
+        child: Center(
+            child: Text(
+          text,
+          style: TextStyle(
+              color: getColor(text), fontSize: 30, fontWeight: FontWeight.bold),
+        )),
+      ),
+    );
+  }
+
+  getColor(String text) {
+    if (text == "/" ||
+        text == "*" ||
+        text == "+" ||
+        text == "-" ||
+        text == "C" ||
+        text == "(" ||
+        text == ")") {
+      return Color.fromARGB(255, 252, 100, 100);
+    }
+    return Colors.white;
+  }
+
+  getBgColor(String text) {
+    if (text == "AC") {
+      return Color.fromARGB(255, 252, 100, 100);
+    }
+    if (text == "=") {
+      return Color.fromARGB(255, 104, 204, 159);
+    }
+    return Colors.black;
+  }
+
+  handleButton(String text) {
+    if (text == "AC") {
+      userInput = "";
+      result = "0";
+    }
+    if (text == "C") {
+      if (userInput.isNotEmpty) {
+        userInput = userInput.substring(0, userInput.length - 1);
+        return;
+      } else {
+        return null;
+      }
+    }
+
+    if (text == "=") {
+      result = calculate();
+      userInput = result;
+      if (userInput.endsWith(".0")) {
+        userInput = userInput.replaceAll(".0", "");
+      }
+      if (result.endsWith(".0")) {
+        result = result.replaceAll(".0", "");
+        return;
+      }
+    }
+
+    userInput = userInput + text;
+  }
+
+  String calculate(){
+    try{
+      var exp = Parser().parse(userInput);
+      var evaluation = exp.evaluate(EvaluationType.REAL, ContextModel());
+      return evaluation.toString();
+    }catch(e){
+      return "Error";
+    }
+  }
+}
